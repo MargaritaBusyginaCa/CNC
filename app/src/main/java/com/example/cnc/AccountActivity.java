e2cf08c580a381b65098f35802e072a8c705b786
MargaritaCa <busy0001@algonquinlive.com>
2021-02-01 15:31:54 -0500

    PDF file(checklist) is readable now

---
+++ app/src/main/java/com/example/cnc/AccountActivity.java
@@ -0,0 +1,21 @@
+package com.example.cnc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class AccountActivity extends AppCompatActivity {
+Button bt_checklist;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+        bt_checklist=findViewById(R.id.check_list_bt);
+        bt_checklist.setOnClickListener(click->{
+            Intent intent=new Intent(this, CheckList_pdf_reader.class);
+            startActivity(intent);
+        });
+    }
+}
