02716b670b63794b7056b579998debe6bfc80153
MargaritaCa <busy0001@algonquinlive.com>
2021-02-01 13:52:03 -0500

    Two activities added. Welcome page and Login page. Intent object works and redirects to another activity

---
+++ app/src/main/java/com/example/cnc/LoginPageActivity.java
@@ -0,0 +1,22 @@
+package com.example.cnc;
+import android.os.Bundle;
+
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPageActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+
+
+        // Enables Always-on
+
+    }
+}
